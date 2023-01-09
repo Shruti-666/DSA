89f38bd50c5d6e41a7c70301065b55efc478974d
Shruti Vats <92461435+Shruti-666@users.noreply.github.com>
2023-01-09 11:24:12 +0530

    Add files via upload

---
+++ dsa_day1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5]={7,12,6,4,1,5};
+    cout<<sort(arr,arr+n);
+    
+    return 0;
+}
---
+++ dsa_day2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    char word[10];     
+    cin.get(word,10);
+    int i=0;
+    // for(int j=0;j<10;j++){
+
+    //     if(word[i]!=0){
+    //         i++;
+    //     }
+    // }
+    int j=0;
+    while(*word[i+j]!='/0'){
+        i++;
+
+    }
+    cout<<i<<endl;
+
+    return 0;
+}
